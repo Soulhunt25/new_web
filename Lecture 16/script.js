bb2de2df4015834e03f56ad574cf18ed5ff095fa
Soulhunt25 <0105cs221134@oriemtal.ac.in>
2024-07-12 18:52:51 +0530

    arrays,loop,function

---
+++ Lecture 16/script.js
@@ -0,0 +1,101 @@
+// var a  // value undefined
+
+// var a = null; // value null 
+// console.log(a);
+
+
+
+
+/// array
+
+// let array = [1,2,3,4,5,6,'string',true];
+
+// console.log(array[6]);
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+// print the index of the array
+// for (let i in array){
+//     console.log(i);
+// }
+
+// print the values of the array
+// for (let i of array){
+//     console.log(i);
+// }
+
+
+
+
+
+/// if else
+
+// let a = 80;
+// if (a < 50) {   // condotion if false
+//     console.log('hello');
+// }
+// else{
+//     console.log('hiiiiiii');
+// }
+
+// let b = 50;
+// if (b < 80) {   // conditio is true
+//     console.log('hello');
+// }
+// else{
+//     console.log('hiiiiiii');
+// }
+
+// let c = true 
+// c ? console.log('true') : console.log('false')
+
+
+
+
+/// && || (and & or operator)
+
+// let a = 20;
+// let b = 30;
+// if (a == 20 && b === 30) {
+//     console.log('true');
+// }
+
+// if (a == 10 || b == 30) {
+//     console.log('one of the condition is true');
+// }
+
+
+
+
+////  Functions
+
+// function add(){
+//     console.log('function called');
+// }
+
+// add()
+
+
+// const add = () => {
+//     console.log('function called');
+// }
+
+// add()
+
+// function add(){
+//     return 2;
+// }
+
+
+// console.log(add());
+
+// function add(a,b) {
+//     return a + b;
+// }
+
+// console.log(add(5,6));
+
+// let add = (a,b) => a+b;
+// console.log(add(6,7));
