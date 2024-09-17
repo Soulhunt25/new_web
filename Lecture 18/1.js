073f2d414343145ee1d74fa885f0c11ac1934c3b
Soulhunt25 <0105cs221134@oriemtal.ac.in>
2024-09-17 19:06:16 +0530

    Array functions & Object

---
+++ Lecture 18/1.js
@@ -0,0 +1,83 @@
+let arr = [2,3,5,4,8,5,6,5];
+
+// arr.forEach((a,b,c) => {
+    // console.log(a);  // returns value
+    // console.log(b);  // returns index
+    // console.log(c);  // returns array
+// });
+
+// Array Functions............................................................
+
+// let arr =[20,32,35,45,78];
+// console.log(arr);
+// arr.pop(); // removes the value form the last
+// arr.push(12); // add a value in the last
+// arr.shift();  // removes first 
+// arr.unshift(100);  // removes last
+// console.log(arr.length);
+// console.log(arr);
+// console.log(arr.includes(5)); // checks weater the value exist in an array or not
+
+// let newArr = arr.map((a,b,c)=>{
+//     return a * 5;
+// });
+
+// console.log(newArr);
+
+
+// find -- filter ............................................................
+
+// let newArr = arr.find((a)=>{
+//     return a == 5
+// });
+
+// console.log(newArr);
+
+// let newArr1 = arr.filter((a)=>{
+//     return a == 5
+// });
+
+// console.log(newArr1);
+
+// reduce functrion
+// a -> stores value
+// b -> ittrates value of array
+// c -> index of value
+// d -> is array itself
+
+
+// let newArr = arr.reduce((a,b,c,d)=> {
+//     return a+b;
+// });
+
+// console.log(newArr);
+
+// let newArr = arr.filter((a)=> {
+//     return (a % 2 != 0) && (a > 3)
+// }).reduce((a,b)=>{
+//     return a+b;
+// });
+
+// console.log(newArr);
+
+
+// let arr = [
+//     {
+//         id : 0,
+//         name : 'Pranjal'
+//     },
+//     {
+//         id : 1,
+//         name : 'Kritika'
+//     },
+//     {
+//         id : 2,
+//         name : 'Muskan'
+//     }
+// ]
+
+// arr.map((a)=>{
+//     console.log(a.id);
+// })
+
+
