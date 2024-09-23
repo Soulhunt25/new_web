0034ad357ee4da21bf390051275c0287ff786174
Soulhunt25 <0105cs221134@oriemtal.ac.in>
2024-09-23 20:51:48 +0530

    DOM

---
+++ Lecture 22/script.js
@@ -0,0 +1,65 @@
+// const h2 = document.querySelectorAll('h2');
+
+
+// for (let i of h2) {
+//     i.style.color='red';    
+// }
+
+// ........................................................
+
+// const h2 = document.querySelector('h2');
+// console.log(h2.innerText);
+// console.log(h2.innerText="youuu");
+
+// ........................................................
+
+// let div = document.querySelector('div');
+// let h2 = document.createElement('h2');
+// let h3 = document.createElement('h3');
+// h2.innerText='hello world';
+// h3.innerText='hello world';
+
+// div.append(h2,h3)
+// console.log(div,'sdfghjkl');
+
+// ........................................................
+
+// let a = document.querySelector('a');
+
+// a.getAttribute('herf');
+
+// a.setAttribute('href','https://www.flipkart.com/');
+
+// ........................................................
+
+// let arr = [ 'https://images.unsplash.com/photo-1726593243881-847529d40d18?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//             'https://images.unsplash.com/photo-1726519372376-6825c67bef15?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//             'https://images.unsplash.com/photo-1726608708346-165c730d3f30?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//             'https://plus.unsplash.com/premium_photo-1724873640555-650bbca38ba8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'];
+
+// let img = document.querySelector('img');
+// let num = 0;
+
+// img.style.height ='700px';
+// img.style.width ='500px';
+
+
+
+// setInterval(() => {
+//     img.setAttribute('src', arr[num]); 
+//     num = (num + 1) % arr.length;     
+// }, 1000);
+
+// console.log(img);
+
+// ........................................................
+
+// let btn = document.querySelector('button');
+
+// btn.onclick = function fun1() {
+//     console.log('hiiii'); 
+// }
+
+// btn.addEventListener('click',()=>{
+//     console.log('hello');
+// })
---
+++ Lecture 23/script.js
@@ -0,0 +1,50 @@
+// let btn = document.querySelector('button');
+// let body = document.querySelector('body');
+// let h2 = document.querySelector('h2');
+// btn.addEventListener('click',()=>{
+//     body.style.backgroundColor = 'red'
+//     h2.innerText='hello'
+// })
+
+//.......................................................................................
+
+// let inp = document.querySelector('input');
+// let h2 = document.querySelector('h2');
+
+// inp.addEventListener('input',(e)=>{
+//     console.log(e.target.value);
+//     h2.innerText = e.target.value;
+// })
+
+//.......................................................................................
+
+// let formE = document.querySelector('form');
+
+// formE.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+//     // console.log(formE.elements[0].value);
+
+//     for (const i of formE.elements) {
+//         console.log(i.value);
+//     }
+// })
+
+//.......................................................................................
+
+// let inp = document.querySelector('input');
+// let btn = document.querySelector('button');
+// let ul = document.querySelector('ul');
+
+// let arr = []
+
+// btn.addEventListener('click',()=>{
+//     let data = inp.value;
+//     let li = document.createElement('li');
+//     li.innerText = data;
+//     ul.append(li);
+//     inp.value = '';
+// })
+
+//.......................................................................................
+
+
